d57b05b60e43c22fabda515f570d213cce07dfa2
cashmere <cashmere@autistici.org>
2025-11-10 18:05:46 +0100

    add cashmere.rs to nixwebr.ing

--- webring.nix
+++ webring.nix
@@ -36,4 +36,5 @@
     { name = "hyperpastel"; site = "https://hyperpas.tel/"; config = "https://github.com/hyperpastel/system"; }
     { name = "liv.town"; site = "https://liv.town"; config = "https://code.liv.town/liv/nixos-config"; }
     { name = "adamperkowski"; site = "https://adamperkowski.dev"; config = "https://github.com/adamperkowski/dotfiles"; }
+    { name = "cashmere"; site = "https://cashmere.rs"; }
 ]
