3effd5597999d25853db024855b3312c30b7212e
santoshxshrestha <santoshxshrestha@gmail.com>
2026-03-14 16:45:25 +0545

    update: rename my name

--- webring.nix
+++ webring.nix
@@ -32,7 +32,7 @@
     { name = "stef"; site = "https://stefdp.com"; config = "https://github.com/Stef-00012/dots"; }
     { name = "aubun"; site = "https://nixfox.ca/"; config = "https://git.nixfox.ca/Bun/nixos-config"; }
     { name = "zackerthescar"; site = "https://zackerthescar.com"; config = "https://github.com/zackerthescar/nix-config"; }
-    { name = "santoshxshrestha"; site = "https://santoshxshrestha.tech"; config = "https://github.com/santoshxshrestha/nixos"; }
+    { name = "vimlinuz"; site = "https://santoshxshrestha.tech"; config = "https://github.com/vimlinuz/nixos"; }
     { name = "hyperpastel"; site = "https://hyperpas.tel/"; config = "https://github.com/hyperpastel/system"; }
     { name = "liv.town"; site = "https://liv.town"; config = "https://code.liv.town/liv/nixos-config"; }
     { name = "koi"; site = "https://koi.rip"; config = "https://codeberg.org/koibtw/dotfiles"; }
